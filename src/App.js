defc42a5470a7b331ceaa072135cefc292c93a22
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 <dohyun911@hanmail.net>
2023-04-29 14:03:55 +0900

    react props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 // 함수형 컴포넌트
-function Header() {
+function Header(props) {
+  console.log('props',props, props.title);
   return <header>
-     <h1><a href='/'>WEB</a></h1>
+     <h1><a href='/'>{props.title}</a></h1>
   </header>
 }
-function Nav() {
+function Nav(props) {
+  const lis = [
+    <li><a href='/read/1'>html</a></li>,
+    <li><a href='/read/1'>css</a></li>,
+    <li><a href='/read/1'>js</a></li>
+  ]
+  for(let i=0; i <props.topics; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href>{'/read/'+t.id}</a>{t.title}</li>)
+  }
   return <nav>
      <ol>
-        <li><a href='/read/1'>html</a></li>
-        <li><a href='/read/1'>css</a></li>
-        <li><a href='/read/1'>js</a></li>
+      {lis}
       </ol>
   </nav>
 } 
-function Article() {
+function Article(props) {
   return <article>
-     <h2>Welcome</h2>
-        Hello, Web
+     <h2>{props.title}</h2>
+     {props.body}
+        
   </article>
 }
 
 function App() {
+  const topics = [
+   {id: 1, title: 'html', body: 'html is...'},
+   {id: 2, title: 'css', body: 'css is...'},
+   {id: 3, title: 'javascript', body: 'javascript is...'}
+  ]
   return (
     <div>
+    <Header title="REACT"></Header>
     <Header></Header>
     <Header></Header>
-    <Header></Header>
-    <Nav></Nav>
-    <Article></Article>
+    <Nav topics="{topics}"></Nav>
+    <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
 }
